14cbeba146a3efcfc9fba194f4df8e08bb0d3411
alexpien <alexpien@alexpien-Lenovo-Z580.(none)>
2013-04-06 19:38:26 -0400

    adsfasd

--- index.php
+++ index.php
@@ -55,27 +55,27 @@
         <a href="#other">other</a>
       </div>
     </div>
-    <div id="#home_sec" display="none">
+    <div id="#home_sec" style="display:none;">
       <h1>
         Welcome to Clapp. Login to facebook to get started.
       </h1>
     </div>
-    <div id="#classes_sec" display="none">
+    <div id="#classes_sec" style="display:none;">
       <h1>
         Welcome to Clapp2. Login to facebook to get started.
       </h1>
     </div>
-    <div id="#friends_sec" display="none">
+    <div id="#friends_sec" style="display:none;">
       <h1>
         Welcome to Clapp3. Login to facebook to get started.
       </h1>
     </div>
-    <div id="#about_sec" display="none">
+    <div id="#about_sec" style="display:none;">
       <h1>
         Welcome to Clapp4. Login to facebook to get started.
       </h1>
     </div>
-    <div id="#other_sec" display="none">
+    <div id="#other_sec" style="display:none;">
       <h1>
         Welcome to Clapp5. Login to facebook to get started.
       </h1>
