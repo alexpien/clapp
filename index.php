42e3c3486a78a43ed70bed0e41bc14b7e6b774d1
Howard Chung <howardc93@gmail.com>
2013-04-06 18:24:37 -0400

    test

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
       FB.api('/me', function(response) {
         alert('Good to see you, ' + response.name + '.'
         	+'You are from ' + response.hometown.name + '.'
-        	+'You go to ' + response.education[response.education.length].name + '.');
+        	+'You go to ' + response.education.education[response.education.length].name + '.');
 
     });
   }
