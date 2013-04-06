099d9fb65accbd93f9055f244594f60d671c5e79
alexpien <alexpien@alexpien-Lenovo-Z580.(none)>
2013-04-06 19:56:41 -0400

    adfasd

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <title>Clapp</title>
     <link rel="stylesheet" href="stylesheets/styles.css" type="text/css">
     <link rel="stylesheet" href="stylesheets/fonts.css" type="text/css">
+    <link rel="Shortcut Icon" href="images/favicon.ico">
     <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
     <script type="text/javascript" src="javascript/jquery-1.9.1.min.js"></script>
     <script type="text/javascript" src="javascript/script.js"></script>
