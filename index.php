cbe8bd5947fa9a70a1d45fbef8bb72d791aad25d
alexpien <alexpien@alexpien-Lenovo-Z580.(none)>
2013-04-06 18:55:46 -0400

    adsfasd

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
   <div id="top">
     
   </div>
-  <div class="fb-login-button" data-show-faces="true" data-width="200" data-max-rows="1">Login with Facebook</div>
+  <div class="fb-login-button" data-show-faces="true" data-width="200" data-max-rows="1" style="position:fixed">Login with Facebook</div>
   <button type="button" onclick="testAPI()">Test API</button>
   <p id="displayInfo">
 
