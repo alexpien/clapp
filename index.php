04dfab7e9b2310f418b326a285a7fabceece40a8
alexpien <alexpien@alexpien-Lenovo-Z580.(none)>
2013-04-06 15:57:55 -0400

    hihih

--- index.php
+++ index.php
@@ -2,13 +2,22 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <title>
+    Clapp
+  </title>
+  <link rel="stylesheet" href="stylesheets/styles.css" type="text/css">
+  <link href='http://fonts.googleapis.com/css?family=Fjalla+One|Open+Sans+Condensed:300|Open+Sans:300' rel='stylesheet' type='text/css'/>
+
+
 </head>
 <body>
-  <header>
-  Welcome to this shit yo
-  </header>
-  <h1>
-    hello
-  </h1>
+  <div id="wrapper">
+    <header>
+      Welcome to this shit yo 
+    </header>
+    <h1>
+      hello
+    </h1>
+  </div>
 </body>
 </html>
