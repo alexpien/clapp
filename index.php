a4fc1c5b6619f4e08fcac072d70d7921fa8f36b7
Howard Chung <howardc93@gmail.com>
2013-04-06 16:48:11 -0400

    test

--- index.php
+++ index.php
@@ -79,26 +79,5 @@ function testAPI() {
       hello
     </h1>
 
-  <div class="list">
-        <h3>A few of your friends</h3>
-        <ul class="friends">
-          <?php
-            foreach ($friends as $friend) {
-              // Extract the pieces of info we need from the requests above
-              $id = idx($friend, 'id');
-              $name = idx($friend, 'name');
-          ?>
-          <li>
-            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
-              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
-              <?php echo he($name); ?>
-            </a>
-          </li>
-          <?php
-            }
-          ?>
-        </ul>
-      </div>
-
 </body>
 </html>
