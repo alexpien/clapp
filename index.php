58a8b88c463a69159f16a3774d346bc6e8231ee5
Howard Chung <howardc93@gmail.com>
2013-04-06 17:52:31 -0400

    test

--- index.php
+++ index.php
@@ -20,27 +20,14 @@ function testAPI() {
     });
   }
 
-  // Additional JS functions here
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '358797270908365', // App ID
-      channelUrl : '//blooming-reef-3850.herokuapp.com/channel.html', // Channel File
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to accdess the session
-      xfbml      : true  // parse XFBML
-    });
-
- });
-};
-
   // Load the SDK Asynchronously
-  (function(d){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     ref.parentNode.insertBefore(js, ref);
-   }(document));
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=358797270908365";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 </script>
 
