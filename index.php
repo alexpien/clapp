6e029387ce59693a09354e9cbdfe9c1d982ce0ac
Kevin Jian <kjian279@gmail.com>
2013-04-07 08:07:51 -0400

    tasdf

--- index.php
+++ index.php
@@ -105,10 +105,10 @@ $db = new PDO($dsn);
                         Welcome to <span style="font-family:'Lobster'; font-size:20px; font-color:#333;">clapp</span>, the best app to connect with your classmates. To begin, enter your classes on the following page. 
                       </p>
                     </div>
+                    <br><br>
                     <div id="button" style="text-align:center">
                       <a href="#classes">Begin</a>
                     </div>
-                    <br><br>
                           <!-- list of current classes, if any -->
                     <?php } 
                     else { ?>
