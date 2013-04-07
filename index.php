de8396d8a6fb86c00e7dbabfa96161aadfda0c98
alexpien <alexpien@alexpien-Lenovo-Z580.(none)>
2013-04-07 04:59:43 -0400

    asdfsd

--- index.php
+++ index.php
@@ -127,7 +127,7 @@ $db = new PDO($dsn);
                               <input id="section" name="course" placeholder=" Course #" required>
 
                               <input type="submit" value="+"/>
-                              <input type="hidden" name="name" value=<?$userInfo['name']?>>
+                              <input type="hidden" name="name" value=<?=$userInfo['name']?>>
                       </form>
                     </div>
                           <!-- list of current classes, if any -->
