f900123057a42963353d4e2b20d7fb768d75369e
alexpien <alexpien@alexpien-Lenovo-Z580.(none)>
2013-04-07 10:38:50 -0400

    aasdf

---
+++ javascript/script.js
@@ -0,0 +1,41 @@
+$(window).load(function(){
+
+
+	subpage=window.location.hash.substring(1);
+	if(subpage!=""){
+		$("#"+subpage+"_sec").slideDown();
+	}else{
+		$("#home_sec").slideDown();
+	}
+
+
+
+
+
+	$("#nav a, #logo a,#button a").click(function(){
+		target=$(this).attr("href");
+		$("#main>div").slideUp();
+		$(target+"_sec").slideDown();
+	
+	});
+
+	$("#classes_sec a").click(function(){
+		
+		$("#classwrapper>div").slideUp();
+		target=$(this).attr("id");
+		
+		$(target).slideDown();
+
+
+	});
+
+	$("div.profile").bind("mousemove",function(event){
+		targ=(this).attr("id")
+		alert(targ);
+		$("#mouseover"+targ).css({	top:event.pageY +5 +"px",left:event.pageX+5+"px"}).show();
+		}).bind("mouseout",function(){$("#mouseover"+targ).hide();
+	});
+
+
+
+});
