e1d24b50a73eb177304cbd14948f5e504fc24aad
alexpien <alexpien@alexpien-Lenovo-Z580.(none)>
2013-04-07 08:56:31 -0400

    asdfasdfads

--- index.php
+++ index.php
@@ -29,7 +29,6 @@ $db = new PDO($dsn);
     <link rel="Shortcut Icon" href="images/favicon.ico">
     <link type='text/css' rel='stylesheet' href='https://fonts.googleapis.com/css?family=Lobster'/>
     <link type='text/css' rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'/>
-    <link type="text/css" href="inputstyle.css" rel="stylesheet"/>
 
     <script type="text/javascript" src="javascript/jquery-1.9.1.min.js"></script>
     <script type="text/javascript" src="javascript/script.js"></script>
