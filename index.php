20407fd2f95139b5bf64f062065403cefc77e2c9
Howard Chung <howardc93@gmail.com>
2013-04-07 03:40:27 -0400

    test

--- index.php
+++ index.php
@@ -10,6 +10,16 @@ $facebook = new Facebook(array(
 
 $userId = $facebook->getUser();
 
+
+$dsn = "pgsql:"
+    . "host=ec2-23-21-161-153.compute-1.amazonaws.com;"
+    . "dbname=dfnau2c20ikt1v;"
+    . "user=qqldptzbgskfay;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=3aCt96iydyR59_GmRL2ltLaXU3";
+$db = new PDO($dsn);
+
 ?>
 
 <!DOCTYPE html>
